59a3e9469937432aca6c7bfb8fee9141e5cbd6c9
Cristian FLORUTA <florutacristian@gmail.com>
2023-01-08 17:53:02 +0200

    .game_over added to the class

--- scoreBoard.py
+++ scoreBoard.py
@@ -8,7 +8,7 @@ class Scoreboard(Turtle):
 
     def __init__(self):
         super().__init__()
-        self.score = 0
+        self.score = 1
         self.penup()
         self.hideturtle()
         self.goto(-260, 260)
@@ -18,3 +18,7 @@ class Scoreboard(Turtle):
         self.score += 1
         self.clear()
         self.write(f"Level {self.score}", align="left", font=FONT)
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("GAME OVER", align="center", font=FONT)
