2316e846aad6b52597971d5c4c2cd6f952616ac5
Cristian FLORUTA <florutacristian@gmail.com>
2023-01-08 17:24:22 +0200

    create_car and move_car methods added

--- carManager.py
+++ carManager.py
@@ -1,27 +1,30 @@
 from turtle import Turtle
-from random import choice
+from random import choice, randint
 
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
-class CarManager(Turtle):
+class CarManager:
 
     def __init__(self):
-        super().__init__()
-        self.shape("square")
-        self.shapesize(stretch_wid=1, stretch_len=3)
-        self.color(choice(COLORS))
-        self.penup()
+        self.all_cars = []
 
-        self.goto(280, STARTING_MOVE_DISTANCE)
+    def create_car(self):
+        chance_to_create_car = randint(1, 6)
+        if chance_to_create_car == 1:
+            new_car = Turtle()
+            new_car.shape("square")
+            new_car.shapesize(stretch_wid=1, stretch_len=2)
+            new_car.color(choice(COLORS))
+            new_car.penup()
+            y_coord = randint(-250, 250)
+            new_car.goto(280, y_coord)
+            self.all_cars.append(new_car)
 
-    def move_forward(self):
-        x_coord = self.xcor() - MOVE_INCREMENT
-        y_coord = self.ycor()
-        self.goto(x_coord, y_coord)
-
-    def new_car(self):
-        pass
-
+    def move_car(self):
+        for car in self.all_cars:
+            x_coord = car.xcor() - MOVE_INCREMENT
+            y_coord = car.ycor()
+            car.goto(x_coord, y_coord)
